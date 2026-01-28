8647da511e0f6bdf3eb3e192ee2a7e255574f260
ams <orphrey@gmail.com>
2026-01-27 22:14:15 -0500

    adds headers to the stripped csvs

--- data_processing_scripts/strip_csv.py
+++ data_processing_scripts/strip_csv.py
@@ -10,19 +10,21 @@ if __name__ == '__main__':
     args = parser.parse_args()
     
     keep_lines = []
-
+    fields = None    
     with open(args.filepath, 'r') as infile:
         reader = csv.DictReader(infile)
         prev_line = None
         for line in reader:
             if prev_line is None:
                 prev_line = line
+                fields = reader.fieldnames
             else:
                 if (prev_line['logic0'] != line['logic0']) or (prev_line['logic1'] != line['logic1']):
                     keep_lines.append(line)
             prev_line = line
     
     with open(args.filepath.replace('.csv', '_stripped.csv'), 'w') as outfile:
-        writer = csv.DictWriter(outfile, reader.fieldnames)
+        writer = csv.DictWriter(outfile, fields)
+        writer.writeheader()
         writer.writerows(keep_lines)
 
